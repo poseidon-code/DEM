2dac3fa3453bcb3483c103f61d0ca5b8107f63a0
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2026-01-02 10:00:00 +0530

    add: 3x3 DEM Map

--- include/DEM/Map.hpp
+++ include/DEM/Map.hpp
@@ -22,77 +22,105 @@ Email : pritamhalder.portfolio@gmail.com
 
 #pragma once
 
+#include <array>
 #include <bit>
 #include <cstdint>
 #include <filesystem>
+#include <limits>
 #include <map>
+#include <optional>
+#include <ranges>
 #include <regex>
 #include <string>
+
 
 #include "DEM.hpp"
 
 
-template <dem_datatype T, std::endian endianness = std::endian::native>
+template <dem_datatype T, std::endian E = std::endian::native>
 class Map {
 public:
-    using Grid = std::map<Coordinate, std::pair<typename DEM<T, endianness>::Type, std::filesystem::path>>;
+    struct GridAvailableEntry {
+        std::filesystem::path path;
+        DEM<T, E>::Type type;
+    };
+
+    struct Tile {
+        Coordinate id;
+        std::optional<DEM<T, E>> dem;
+    };
+
+    using Grid = std::array<Tile, 9>;
+    using GridAvailable = std::map<Coordinate, GridAvailableEntry>;
+
 
     Map() = default;
 
-    Map(const Grid& grid) {
-        if (grid.empty()) {
-            throw std::runtime_error("map grid is empty\n");
+    Map(const GridAvailable& available) {
+        if (available.empty()) {
+            throw std::runtime_error("available DEM list is empty\n");
         }
 
-        for (auto m = grid.cbegin(); m != grid.cend(); ++m) {
-            std::filesystem::path filepath = m->second.second;
-
-            if (!std::filesystem::exists(filepath)) {
-                std::string e = "'" + filepath.string() + "' file doesn't exists\n";
-                throw std::runtime_error(e);
+        for (auto entry = available.cbegin();  entry != available.cend(); entry++) {
+            if (!std::filesystem::exists(entry->second.path) || !std::filesystem::is_regular_file(entry->second.path)) {
+                std::string e = "'" + entry->second.path.string() + "' file doesn't exists or not a regular file\n";
+                throw std::invalid_argument(e);
             }
         }
 
-        this->grid = grid;
-
-        auto first_dem = this->grid.cbegin();
-        this->dem = DEM<T, endianness>(this->grid[first_dem->first].first, this->grid[first_dem->first].second);
+        this->available = std::move(available);
     };
 
 
     ~Map() = default;
 
 
-    const DEM<T, endianness>& get_dem() const {
-        return this->dem;
+    std::optional<std::reference_wrapper<DEM<T, E>>> get_dem(float latitude, float longitude) {
+        Coordinate tile_coordinate{std::floor(latitude), std::floor(longitude)};
+
+        auto tile = std::ranges::find_if(
+            this->map.begin(),
+            this->map.end(),
+            [&](const Tile& t) -> bool { return (t.id == tile_coordinate); }
+        );
+
+        if (tile != this->map.end() && tile->dem) {
+            return std::ref(*tile->dem);
+        }
+
+        return std::nullopt;
     }
 
 
     T altitude(float latitude, float longitude) {
-        if (!this->dem.bounds.within(latitude, longitude)) {
-            if (!this->load(latitude, longitude)) {
-                return this->dem.type.nodata;
-            }
+        if (!this->load_bounds.within(latitude, longitude)) {
+            this->load(latitude, longitude);
         }
 
-        return this->dem.altitude(latitude, longitude);
+        if (auto t_dem = this->get_dem(latitude, longitude)) {
+            return t_dem->get().altitude(latitude, longitude);
+        }
+
+        return std::numeric_limits<T>::lowest();
     };
 
 
     float interpolated_altitude(float latitude, float longitude) {
-        if (!this->dem.bounds.within(latitude, longitude)) {
-            if (!this->load(latitude, longitude)) {
-                return this->dem.type.nodata;
-            }
+        if (!this->load_bounds.within(latitude, longitude)) {
+            this->load(latitude, longitude);
         }
 
-        return this->dem.interpolated_altitude(latitude, longitude);
+        if (auto t_dem = this->get_dem(latitude, longitude)) {
+            return t_dem->get().interpolated_altitude(latitude, longitude);
+        }
+
+        return std::numeric_limits<T>::lowest();
     };
 
 
-    static Grid initialize(const std::filesystem::path& dem_directory_path, size_t nrows, size_t ncols, float cellsize, T nodata) {
-        Map<T, endianness>::Grid grid;
-        std::regex pattern(R"(([-]?\d{1,2}|90)_([-]?\d{1,3}|180)\.bin)");
+    static GridAvailable list_available(const std::filesystem::path& dem_directory_path, size_t nrows, size_t ncols, float cellsize, T nodata) {
+        const std::regex pattern = std::regex(R"(([-]?\d{1,2}|90)_([-]?\d{1,3}|180)\.bin)");
+        GridAvailable result;
 
         try {
             for (const auto& entry : std::filesystem::directory_iterator(dem_directory_path)) {
@@ -101,41 +129,80 @@ public:
                     std::smatch match;
 
                     if (std::regex_match(filename, match, pattern)) {
-                        float latitude = std::stod(match[1]);
-                        float longitude = std::stod(match[2]);
+                        float y = std::stod(match[1]);
+                        float x = std::stod(match[2]);
 
-                        if (
-                            (latitude >= -90 && latitude <= 90)
-                            && (longitude >= -180 && longitude <= 180)
-                        ) {
-                            typename DEM<T, endianness>::Type type(nrows, ncols, latitude, longitude, cellsize, nodata);
-                            grid[{latitude, longitude}] = {type, entry.path().string()};
+                        if ((y >= -90 && y <= 89) && (x >= -180 && x <= 179)) {
+                            typename DEM<T, E>::Type type(nrows, ncols, y, x, cellsize, nodata);
+                            result[{y, x}] = { entry.path(), type };
                         }
                     }
                 }
             }
-        } catch (const std::filesystem::filesystem_error& e) {
+        } catch (const std::exception& e) {
             throw std::runtime_error(std::string(e.what()) + "\n");
         }
 
-        return grid;
-    };
+        return result;
+    }
 
 
 private:
-    DEM<T, endianness> dem;
-    Grid grid;
+    Grid map;
+    Coordinate map_center;
+    GridAvailable available;
+    Bounds load_bounds;
+
+    const std::array<std::pair<float,float>, 9> offsets{{
+        {+1,-1}, {+1,0}, {+1,+1},
+        { 0,-1}, { 0,0}, { 0,+1},
+        {-1,-1}, {-1,0}, {-1,+1}
+    }};
 
 
-    bool load(float latitude, float longitude) {
-        Coordinate grid_coordinate{
+    void load(float latitude, float longitude) {
+        Coordinate tile_coordinate{
             std::floor(latitude),
             std::floor(longitude)
         };
 
-        if (this->grid.count(grid_coordinate) == 0) return false;
-        this->dem = DEM<T, endianness>(this->grid[grid_coordinate].first, this->grid[grid_coordinate].second);
+        if (!this->load_bounds.within(latitude, longitude)) {
+            this->map_center.latitude = tile_coordinate.latitude + (((tile_coordinate.latitude + 1) - tile_coordinate.latitude) / 2.0);
+            this->map_center.longitude = tile_coordinate.longitude + (((tile_coordinate.longitude + 1) - tile_coordinate.longitude) / 2.0);
 
-        return true;
+            this->load_bounds = Bounds{
+                {this->map_center.latitude + 1, this->map_center.longitude - 1},
+                {this->map_center.latitude + 1, this->map_center.longitude + 1},
+                {this->map_center.latitude - 1, this->map_center.longitude - 1},
+                {this->map_center.latitude - 1, this->map_center.longitude + 1}
+            };
+
+
+            for (size_t i = 0; i < 9; i++) {
+                Coordinate c{
+                    tile_coordinate.latitude + this->offsets[i].first,
+                    tile_coordinate.longitude + this->offsets[i].second
+                };
+
+                // tile is already avaialble and loaded
+                auto tile = std::ranges::find_if(this->map.begin(), this->map.end(), [&](Tile t) -> bool {return (t.id == c);});
+                if (tile != this->map.end()) {
+                    this->map[i] = std::move(*tile);
+                    continue;
+                }
+
+                // tile is avaialble but needs to be loaded
+                this->map[i].id = c;
+                auto entry = this->available.find(c);
+                if (entry != this->available.end()) {
+                    this->map[i].dem.emplace(entry->second.type, entry->second.path);
+                    continue;
+                }
+
+                // tile is not avaialble
+                this->map[i].dem.reset();
+                this->map[i].dem = std::nullopt;
+            }
+        }
     };
 };
